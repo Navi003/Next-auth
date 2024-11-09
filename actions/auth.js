a56fb4d600944f72afd3f72242b0b046f71befa0
Navjot Dhiman <dhimannavjot1@gmail.com>
2024-11-09 23:11:14 +0100

    added Lucia and auth.

---
+++ actions/auth.js
@@ -0,0 +1,46 @@
+"use server";
+
+import { hashUserPassword } from "@/lib/hashpassword";
+import { createUser } from "@/lib/user";
+import { redirect } from "next/navigation";
+
+//FormData is 2nd argument
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Please enter a valid email address";
+  }
+
+  if (password.trim().length < 8) {
+    errors.password = "Password must be atleast 8 charachters long";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+    };
+  }
+
+  // Store it in Data base
+  const hashedPassword = hashUserPassword(password);
+
+  try {
+    createUser(email, hashedPassword);
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email: "It Seems like an account for the chosen email already Exists",
+        },
+      };
+    }
+
+    throw error;
+  }
+
+  redirect("/training");
+}
